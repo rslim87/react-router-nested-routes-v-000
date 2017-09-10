41363abc21a990e939a52049fbec501b3aa69352
Rachel Lim <rachel@Rachels-MacBook-Pro.local>
2017-09-10 19:05:50 -0400

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,11 +1,20 @@
 //./src/containers/MoviesPage.js
+
+
+
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList';
+import MovieShow from './MovieShow';
  
-const MoviesPage = ({ movies }) => 
+const MoviesPage = ({ match, movies }) => 
   <div>
     <MoviesList movies={movies} />
+    <Route path={`${match.url}/:movieId`} component={MovieShow}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Movie from the list.</h3>
+    )}/>
   </div>;
  
 const mapStateToProps = (state) => {
@@ -13,34 +22,5 @@ const mapStateToProps = (state) => {
     movies: state.movies
   };
 }
- 
-// export default connect(mapStateToProps)(MoviesPage);
-
-// Failed to compile.
-
-// Error in ./src/containers/MoviesPage.js
-// Module not found: ./MovieShow in /Users/rachel/Development/code/react-router-nested-routes-v-000/src/containers
-
-//  @ ./src/containers/MoviesPage.js 36:17-39
-// import React from 'react';
-// import { Route } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import MoviesList from '../components/MoviesList';
-// import MovieShow from './MovieShow';
- 
-// const MoviesPage = ({ match, movies }) => 
-//   <div>
-//     <MoviesList movies={movies} />
-//     <Route path={`${match.url}/:movieId`} component={MovieShow}/>
-//     <Route exact path={match.url} render={() => (
-//       <h3>Please select a Movie from the list.</h3>
-//     )}/>
-//   </div>;
- 
-// const mapStateToProps = (state) => {
-//   return {
-//     movies: state.movies
-//   };
-// }
  
 export default connect(mapStateToProps)(MoviesPage);
